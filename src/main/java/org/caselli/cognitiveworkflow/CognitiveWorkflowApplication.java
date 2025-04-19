f00fb7d7d21206e77120bdb8459b04bc4315f9ce
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-04-19 21:49:40 +0200

    first commit

---
+++ src/main/java/org/caselli/cognitiveworkflow/CognitiveWorkflowApplication.java
@@ -0,0 +1,13 @@
+package org.caselli.cognitiveworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognitiveWorkflowApplication.class, args);
+    }
+
+}
