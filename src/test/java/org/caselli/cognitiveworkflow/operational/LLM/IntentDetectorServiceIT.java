51053a1f1799b9d32afcf9efb42badba4a14ef05
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-05-05 22:08:30 +0200

    Integration test of IntentDetector

--- src/test/java/org/caselli/cognitiveworkflow/operational/LLM/IntentDetectorServiceIT.java
+++ src/test/java/org/caselli/cognitiveworkflow/operational/LLM/IntentDetectorServiceIT.java
@@ -3,17 +3,16 @@ package org.caselli.cognitiveworkflow.operational.LLM;
 import org.caselli.cognitiveworkflow.knowledge.MOP.IntentMetamodelService;
 import org.caselli.cognitiveworkflow.knowledge.MOP.IntentSearchService;
 import org.caselli.cognitiveworkflow.knowledge.model.intent.IntentMetamodel;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -29,36 +28,103 @@ class IntentDetectorServiceIT {
 
     @Autowired private IntentMetamodelService intentMetamodelService;
 
+    // Mock intent metamodel service
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public IntentMetamodelService intentMetamodelService() {
+            return mock(IntentMetamodelService.class);
+        }
+
+        @Bean
+        public IntentSearchService intentSearchService() {
+            return mock(IntentSearchService.class);
+        }
+    }
+
+    private static List<IntentMetamodel> mockIntentMetamodels;
+    private static IntentMetamodel bookFightMockIntent;
+    private static IntentMetamodel bookHotelMockIntent;
+
+    @BeforeAll()
+    static void setUp() {
+        mockIntentMetamodels = new ArrayList<>();
+        bookFightMockIntent = new IntentMetamodel();
+        bookFightMockIntent.setId("1");
+        bookFightMockIntent.setName("BOOK_FLIGHT");
+        bookFightMockIntent.setDescription("Book a flight to a destination");
+        mockIntentMetamodels.add(bookFightMockIntent);
+
+        bookHotelMockIntent = new IntentMetamodel();
+        bookHotelMockIntent.setId("2");
+        bookHotelMockIntent.setName("BOOK_HOTEL");
+        bookHotelMockIntent.setDescription("Book a hotel in a destination");
+        mockIntentMetamodels.add(bookHotelMockIntent);
+    }
+
 
     @Test
     void shouldHandleMultipleIntentsProperly() {
 
         String userRequest = "I want to book a flight to Paris for tomorrow";
 
+        when(intentMetamodelService.findMostSimilarIntent(userRequest)).thenReturn(mockIntentMetamodels);
 
-        // MOCK INTENTS RAG RESULTS
-        List<IntentMetamodel> existingIntent = new ArrayList<IntentMetamodel>();
-        IntentMetamodel bookFightIntent = new IntentMetamodel();
-        bookFightIntent.setId("1");
-        bookFightIntent.setDescription("Book a flight to a destination");
-        bookFightIntent.setName("BOOK_FLIGHT");
-        existingIntent.add(bookFightIntent);
+        IntentDetectorResult result = intentDetectorService.detect(userRequest);
+        System.out.println(result);
 
-        IntentMetamodel bookHotelIntent = new IntentMetamodel();
-        bookHotelIntent.setId("2");
-        bookHotelIntent.setDescription("Book a hotel in a destination");
-        bookHotelIntent.setName("BOOK_HOTEL");
-        existingIntent.add(bookHotelIntent);
+        assertNotNull(result);
+        assertEquals(bookFightMockIntent.getId(), result.getIntentId());
+        assertEquals(bookFightMockIntent.getName(), result.getIntentName());
+        assertFalse(result.isNew());
+        assertTrue(result.getConfidence() >= 0);
 
-        when(intentMetamodelService.findMostSimilarIntent(userRequest)).thenReturn(existingIntent);
+        // Check variables. At list Paris and Tomorrow should be present among the values.
+        Map<String, Object> variables = result.getUserVariables();
+        assertTrue(variables.values().stream().anyMatch(value -> value.toString().toLowerCase().contains("paris")));
+        assertTrue(variables.values().stream().anyMatch(value -> value.toString().toLowerCase().contains("tomorrow")));
+    }
+
+    @Test
+    void shouldCreateANewIntentIfNotExists(){
+        String userRequest = "I want to book an uber car ride to my home";
+
+        when(intentMetamodelService.findMostSimilarIntent(userRequest)).thenReturn(mockIntentMetamodels);
 
         IntentDetectorResult result = intentDetectorService.detect(userRequest);
 
-        assertEquals(bookHotelIntent.getId(), result.getIntentId());
-        assertEquals(bookHotelIntent.getName(), result.getIntentName());
+        System.out.println(result);
+
+        assertNotNull(result);
+        assertTrue(result.isNew());
+        assertNull(result.getIntentId());
+        // The intent name should include "UBER" in it
+        assertTrue(result.getIntentName().contains("UBER"));
+        // The intent name should be uppercase
+        assertEquals(result.getIntentName(), result.getIntentName().toUpperCase());
 
 
+    }
 
+    @Test
+    void shouldReturnNullIfInputIsNonSense(){
+        String userRequest = "oajadfjaoifj";
 
+        when(intentMetamodelService.findMostSimilarIntent(userRequest)).thenReturn(mockIntentMetamodels);
+
+        IntentDetectorResult result = intentDetectorService.detect(userRequest);
+
+        assertNull(result);
+    }
+
+    @Test
+    void shouldReturnNullIfInputIsNotAnIntent(){
+        String userRequest = "Sometimes I like to eat a book";
+
+        when(intentMetamodelService.findMostSimilarIntent(userRequest)).thenReturn(mockIntentMetamodels);
+
+        IntentDetectorResult result = intentDetectorService.detect(userRequest);
+
+        assertNull(result);
     }
 }
