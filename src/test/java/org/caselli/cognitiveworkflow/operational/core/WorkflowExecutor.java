24e99c6f2874a46e3a57ec42c80b3cb6c227e7c2
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-05-06 17:07:42 +0200

    new WorkflowExecutor unit tests

--- src/test/java/org/caselli/cognitiveworkflow/operational/core/WorkflowExecutor.java
+++ src/test/java/org/caselli/cognitiveworkflow/operational/core/WorkflowExecutor.java
@@ -1,6 +1,5 @@
 package org.caselli.cognitiveworkflow.operational.core;
 
-import lombok.SneakyThrows;
 import org.caselli.cognitiveworkflow.knowledge.model.node.NodeMetamodel;
 import org.caselli.cognitiveworkflow.knowledge.model.node.RestToolNodeMetamodel;
 import org.caselli.cognitiveworkflow.knowledge.model.node.port.PortSchema;
@@ -12,157 +11,204 @@ import org.caselli.cognitiveworkflow.operational.ExecutionContext;
 import org.caselli.cognitiveworkflow.operational.node.NodeInstance;
 import org.caselli.cognitiveworkflow.operational.node.RestToolNodeInstance;
 import org.caselli.cognitiveworkflow.operational.workflow.WorkflowInstance;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @Tag("test")
 class WorkflowExecutorTest {
 
-    private WorkflowExecutor executor;
-    private WorkflowInstance workflowInstance;
-    private RestToolNodeMetamodel nodeMetamodelA;
-    private RestToolNodeMetamodel nodeMetamodelB;
-    private NodeInstance nodeInstanceA;
-    private NodeInstance nodeInstanceB;
-    private WorkflowNode wNodeA;
-    private WorkflowNode wNodeB;
-
-    @SneakyThrows
-    @BeforeEach
-    void setUp() {
-
-        // NODE A
-        nodeMetamodelA = new RestToolNodeMetamodel();
-        nodeMetamodelA.setId("nodeA");
-        nodeMetamodelA.setName("Node A");
-        nodeMetamodelA.setDescription("Node A description");
-        nodeMetamodelA.setType(NodeMetamodel.NodeType.TOOL);
-        nodeMetamodelA.setToolType(RestToolNodeMetamodel.ToolType.REST);
-        nodeMetamodelA.setServiceUri("http://example.com/api");
-
-        RestPort inputPortA_1 = RestPort.resBuilder()
-                .withKey("inputA_1")
-                .withSchema(PortSchema.builder().stringSchema().withRequired(true).build())
+    private RestPort createStringPort(String id) {
+        return RestPort.resBuilder()
+                .withKey(id)
+                .withSchema(PortSchema.builder().withRequired(true).stringSchema().build())
                 .build();
-
-        RestPort inputPortA_2 = RestPort.resBuilder()
-                .withKey("inputA_2")
-                .withSchema(PortSchema.builder().stringSchema().withRequired(true).build())
-                .build();
-
-        RestPort outputPortA = RestPort.resBuilder()
-                .withKey("outputA")
-                .withSchema(PortSchema.builder().stringSchema().withRequired(true).build())
-                .build();
-
-        nodeMetamodelA.setInputPorts(java.util.List.of((RestPort) inputPortA_1, (RestPort) inputPortA_2));
-        nodeMetamodelA.setOutputPorts(java.util.List.of((RestPort) outputPortA));
-
-        nodeInstanceA = mock(RestToolNodeInstance.class);
-        nodeInstanceA.setMetamodel(nodeMetamodelA);
-        when(nodeInstanceA.getMetamodel()).thenReturn(nodeMetamodelA);
-        when(nodeInstanceA.getId()).thenReturn("nodeA");
-        // Mock the behavior of Instance A process that add the output to the context
-        doAnswer(invocation -> {
-            ExecutionContext context = invocation.getArgument(0);
-            context.put("outputA", "valueA");
-            return null;
-        }).when(nodeInstanceA).process(any(ExecutionContext.class));
-
-        // NODE B
-        nodeMetamodelB = new RestToolNodeMetamodel();
-        nodeMetamodelB.setId("nodeB");
-        nodeMetamodelB.setName("Node B");
-        nodeMetamodelB.setDescription("Node B description");
-        nodeMetamodelB.setType(NodeMetamodel.NodeType.TOOL);
-        nodeMetamodelB.setToolType(RestToolNodeMetamodel.ToolType.REST);
-
-        RestPort inputPortB = RestPort.resBuilder()
-                .withKey("inputB")
-                .withSchema(PortSchema.builder().stringSchema().withRequired(true).build())
-                .build();
-
-        RestPort outputPortB = RestPort.resBuilder()
-                .withKey("outputB")
-                .withSchema(PortSchema.builder().stringSchema().withRequired(true).build())
-                .build();
-
-
-        nodeMetamodelB.setInputPorts(java.util.List.of(inputPortB));
-        nodeMetamodelB.setOutputPorts(java.util.List.of(outputPortB));
-
-        nodeInstanceB = mock(RestToolNodeInstance.class);
-        nodeInstanceB.setMetamodel(nodeMetamodelB);
-        when(nodeInstanceB.getMetamodel()).thenReturn(nodeMetamodelB);
-        when(nodeInstanceB.getId()).thenReturn("nodeB");
-
-
-        // Workflow Metamodel
-
-
-        WorkflowMetamodel workflowMetamodel = getWorkflowMetamodel();
-
-        // Workflow Instance
-        workflowInstance = new WorkflowInstance();
-        workflowInstance.setMetamodel(workflowMetamodel);
-        workflowInstance.setId("workflow1");
-        workflowInstance.setNodeInstances(java.util.List.of(nodeInstanceA, nodeInstanceB));
-
-        // Execution Context
-        executor = new WorkflowExecutor(workflowInstance);
-
-
     }
 
-    private WorkflowMetamodel getWorkflowMetamodel() {
-        wNodeA = new WorkflowNode();
-        wNodeA.setId("A");
-        wNodeA.setNodeMetamodelId(nodeMetamodelA.getId());
+    private NodeInstance createNodeInstanceA(String id, List<RestPort> inputPorts, List<RestPort> outputPorts) {
+        RestToolNodeMetamodel nodeMetamodel = new RestToolNodeMetamodel();
+        nodeMetamodel.setId(id);
+        nodeMetamodel.setName("name");
+        nodeMetamodel.setDescription("description");
+        nodeMetamodel.setServiceUri("http://localhost:8080/service");
+        nodeMetamodel.setInvocationMethod(RestToolNodeMetamodel.InvocationMethod.POST);
+        nodeMetamodel.setType(NodeMetamodel.NodeType.TOOL);
+        nodeMetamodel.setToolType(RestToolNodeMetamodel.ToolType.REST);
+        nodeMetamodel.setInputPorts(inputPorts);
+        nodeMetamodel.setOutputPorts(outputPorts);
 
-        wNodeB = new WorkflowNode();
-        wNodeB.setId("B");
-        wNodeB.setNodeMetamodelId(nodeMetamodelB.getId());
+        RestToolNodeInstance nodeInstance = mock(RestToolNodeInstance.class);
+        when(nodeInstance.getMetamodel()).thenReturn(nodeMetamodel);
+        when(nodeInstance.getId()).thenReturn(id);
+
+        return nodeInstance;
+    }
 
 
-        WorkflowEdge edge = new WorkflowEdge();
-        edge.setSourceNodeId(wNodeA.getId());
-        edge.setTargetNodeId(wNodeB.getId());
 
-        Map<String,String> bindings = Map.of(
-                "outputA", "inputB"
-        );
-        edge.setBindings(bindings);
-
+    private WorkflowInstance createWorkflowInstance(String id, List<NodeInstance> nodeInstances, List<WorkflowNode> nodes,  List<WorkflowEdge> edges) {
         WorkflowMetamodel workflowMetamodel = new WorkflowMetamodel();
-        workflowMetamodel.setId("workflow1");
-        workflowMetamodel.setName("Workflow 1");
-        workflowMetamodel.setDescription("Workflow 1 description");
-        workflowMetamodel.setNodes(java.util.List.of(wNodeA, wNodeB));
-        workflowMetamodel.setEdges(java.util.List.of(edge));
+        workflowMetamodel.setId(id);
+        workflowMetamodel.setName("name");
+        workflowMetamodel.setDescription("description");
+        workflowMetamodel.setNodes(nodes);
+        workflowMetamodel.setEdges(edges);
         workflowMetamodel.setEnabled(true);
-        return workflowMetamodel;
+
+        WorkflowInstance workflowInstance = new WorkflowInstance();
+        workflowInstance.setId(id);
+        workflowInstance.setNodeInstances(nodeInstances);
+        workflowInstance.setMetamodel(workflowMetamodel);
+        return workflowInstance;
     }
+
+
 
     @Test
-    void testExecute_directBindings() throws Exception {
+    void testExecute_implicitBindings() throws Exception {
+        // A
+        RestPort inputPortA1 = createStringPort("inputA_1");
+        RestPort inputPortA2 = createStringPort("inputA_2");
+        RestPort outputPortA1 = createStringPort("outputA_1");
+        NodeInstance nodeA = createNodeInstanceA("nodeA", List.of(inputPortA1,inputPortA2), List.of(outputPortA1));
 
+        // B
+        RestPort inputPortB = createStringPort("outputA_1");
+        RestPort outputPortB = createStringPort("outputB_1");
+        NodeInstance nodeB = createNodeInstanceA("nodeB", List.of(inputPortB), List.of(outputPortB));
 
+        // WORKFLOW
+        WorkflowNode wNodeA = new WorkflowNode();
+        wNodeA.setId("A");
+        wNodeA.setNodeMetamodelId(nodeA.getMetamodel().getId());
 
+        WorkflowNode wNodeB = new WorkflowNode();
+        wNodeB.setId("B");
+        wNodeB.setNodeMetamodelId(nodeB.getMetamodel().getId());
+
+        WorkflowEdge edge = new WorkflowEdge();
+        edge.setSourceNodeId("A");
+        edge.setTargetNodeId("B");
+
+        WorkflowInstance workflowInstance = createWorkflowInstance("workflow1", List.of(nodeA, nodeB), List.of(wNodeA, wNodeB), List.of(edge));
+
+        // INPUT CONTEXT
         ExecutionContext context = new ExecutionContext();
         context.put("inputA_1", "valueA_1");
         context.put("inputA_2", "valueA_2");
 
-        // Execute the workflow
+        // MOCKING
+        doAnswer(invocation -> {
+            context.put("outputA_1", "valueA_1");
+            return null;
+        }).when(nodeA).process(context);
+
+        // EXECUTE
+        WorkflowExecutor executor = new WorkflowExecutor(workflowInstance);
         executor.execute(context);
 
         // Verify that the nodes were executed in the correct order
-        verify(nodeInstanceA).process(context);
-        verify(nodeInstanceB).process(context);
+        verify(nodeA).process(context);
+        verify(nodeB).process(context);
+    }
 
+    @Test
+    void testExecute_explicitBindings_shouldWork() throws Exception {
+        // A
+        RestPort inputPortA1 = createStringPort("inputA_1");
+        RestPort inputPortA2 = createStringPort("inputA_2");
+        RestPort outputPortA1 = createStringPort("outputA_1");
+        NodeInstance nodeA = createNodeInstanceA("nodeA", List.of(inputPortA1,inputPortA2), List.of(outputPortA1));
+
+        // B
+        RestPort inputPortB = createStringPort("inputB");
+        RestPort outputPortB = createStringPort("outputB_1");
+        NodeInstance nodeB = createNodeInstanceA("nodeB", List.of(inputPortB), List.of(outputPortB));
+
+        // WORKFLOW
+        WorkflowNode wNodeA = new WorkflowNode();
+        wNodeA.setId("A");
+        wNodeA.setNodeMetamodelId(nodeA.getMetamodel().getId());
+
+        WorkflowNode wNodeB = new WorkflowNode();
+        wNodeB.setId("B");
+        wNodeB.setNodeMetamodelId(nodeB.getMetamodel().getId());
+
+        WorkflowEdge edge = new WorkflowEdge();
+        edge.setSourceNodeId("A");
+        edge.setTargetNodeId("B");
+        edge.setBindings(Map.of("outputA_1", "inputB")); // <--------- Explicit binding
+
+        WorkflowInstance workflowInstance = createWorkflowInstance("workflow1", List.of(nodeA, nodeB), List.of(wNodeA, wNodeB), List.of(edge));
+
+        // INPUT CONTEXT
+        ExecutionContext context = new ExecutionContext();
+        context.put("inputA_1", "valueA_1");
+        context.put("inputA_2", "valueA_2");
+
+        // MOCKING
+        doAnswer(invocation -> {
+            context.put("outputA_1", "valueA_1");
+            return null;
+        }).when(nodeA).process(context);
+
+        // EXECUTE
+        WorkflowExecutor executor = new WorkflowExecutor(workflowInstance);
+        executor.execute(context);
+
+        // Verify that the nodes were executed in the correct order
+        verify(nodeA).process(context);
+        verify(nodeB).process(context);
+    }
+
+    @Test
+    void testExecute_explicitBindings_shouldFail() throws Exception {
+        // A
+        RestPort inputPortA1 = createStringPort("inputA_1");
+        RestPort inputPortA2 = createStringPort("inputA_2");
+        RestPort outputPortA1 = createStringPort("outputA_1");
+        NodeInstance nodeA = createNodeInstanceA("nodeA", List.of(inputPortA1,inputPortA2), List.of(outputPortA1));
+
+        // B
+        RestPort inputPortB = createStringPort("inputB");
+        RestPort outputPortB = createStringPort("outputB_1");
+        NodeInstance nodeB = createNodeInstanceA("nodeB", List.of(inputPortB), List.of(outputPortB));
+
+        // WORKFLOW
+        WorkflowNode wNodeA = new WorkflowNode();
+        wNodeA.setId("A");
+        wNodeA.setNodeMetamodelId(nodeA.getMetamodel().getId());
+
+        WorkflowNode wNodeB = new WorkflowNode();
+        wNodeB.setId("B");
+        wNodeB.setNodeMetamodelId(nodeB.getMetamodel().getId());
+
+        WorkflowEdge edge = new WorkflowEdge();
+        edge.setSourceNodeId("A");
+        edge.setTargetNodeId("B");
+
+        WorkflowInstance workflowInstance = createWorkflowInstance("workflow1", List.of(nodeA, nodeB), List.of(wNodeA, wNodeB), List.of(edge));
+
+        // INPUT CONTEXT
+        ExecutionContext context = new ExecutionContext();
+        context.put("inputA_1", "valueA_1");
+        context.put("inputA_2", "valueA_2");
+
+        // MOCKING
+        doAnswer(invocation -> {
+            context.put("outputA_1", "valueA_1");
+            return null;
+        }).when(nodeA).process(context);
+
+        // EXECUTE
+        WorkflowExecutor executor = new WorkflowExecutor(workflowInstance);
+
+        assertThrows(Exception.class, () -> {
+            executor.execute(context);
+        });
     }
 }
