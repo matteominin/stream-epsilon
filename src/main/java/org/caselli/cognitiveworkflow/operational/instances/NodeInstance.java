d32e7480c1110510b4072a8c2e4a64d977c8ca1c
niccolocaselli <caselli.niccolo.developer@gmail.com>
2025-06-11 22:01:38 +0200

    Removed event listener from node instance

--- src/main/java/org/caselli/cognitiveworkflow/operational/instances/NodeInstance.java
+++ src/main/java/org/caselli/cognitiveworkflow/operational/instances/NodeInstance.java
@@ -2,12 +2,10 @@ package org.caselli.cognitiveworkflow.operational.instances;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.caselli.cognitiveworkflow.knowledge.MOP.event.NodeMetamodelUpdateEvent;
 import org.caselli.cognitiveworkflow.knowledge.model.node.NodeMetamodel;
 import org.caselli.cognitiveworkflow.operational.ExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.event.EventListener;
 
 @Setter
 @Getter
@@ -23,14 +21,6 @@ public abstract class NodeInstance {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     public abstract void process(ExecutionContext context) throws Exception;
-
-    @EventListener
-    public void onMetaNodeUpdated(NodeMetamodelUpdateEvent event) {
-        if (this.metamodel != null && event.metamodelId().equals(this.metamodel.getId())) {
-            this.metamodel = event.updatedMetamodel();
-        }
-    }
-
 
     /**
      * Method to handle the refresh of the node
